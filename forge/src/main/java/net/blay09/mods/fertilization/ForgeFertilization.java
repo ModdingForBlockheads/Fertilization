578acef16e325ea4ab960d6f70a313eed9897330
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-01-13 20:36:13 +0100

    feat: Update for Forge 1.21.4

--- forge/src/main/java/net/blay09/mods/fertilization/ForgeFertilization.java
+++ forge/src/main/java/net/blay09/mods/fertilization/ForgeFertilization.java
@@ -1,13 +1,15 @@
 package net.blay09.mods.fertilization;
 
 import net.blay09.mods.balm.api.Balm;
-import net.blay09.mods.balm.api.EmptyLoadContext;
+import net.blay09.mods.balm.forge.ForgeLoadContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Fertilization.MOD_ID)
 public class ForgeFertilization {
-    public ForgeFertilization() {
-        Balm.initialize(Fertilization.MOD_ID, EmptyLoadContext.INSTANCE, Fertilization::initialize);
+    public ForgeFertilization(FMLJavaModLoadingContext context) {
+        final var loadContext = new ForgeLoadContext(context.getModEventBus());
+        Balm.initialize(Fertilization.MOD_ID, loadContext, Fertilization::initialize);
     }
 
 }
